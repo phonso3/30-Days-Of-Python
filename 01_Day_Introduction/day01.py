5adce23ec20ee3e25f0bdc854b4978cf12620608
Alphonso Williams <alphonsowilliams@Alphonsos-MacBook-Pro.local>
2026-08-06 23:43:28 +0900

    Complete Day 2 variables exercises

---
+++ 01_Day_Introduction/day01.py
@@ -0,0 +1,25 @@
+#Day1.
+
+print("Hello World!")
+
+print(2 + 3) # add
+print(5 - 1) # subtract
+print(5 * 3) # multiply
+print(4 + 3) # add
+print(7 - 3) # subtract
+print(21 * 3) # multiply
+print(4 / 2) # division
+print(5 ** 3) # exponential 
+print( 4 % 2) # modulus
+print( 4 // 2) # Ffloor division operator
+
+#Checking data types 
+
+print(type(10)) 
+print(type(3.14)) 
+print(type(1 + 3j)) 
+print(type('Alphonso')) 
+print(type([1, 2, 3])) 
+print(type({'name': 'Alphonso'})) 
+print(type({9.8, 3.14, 2,7}))
+
---
+++ 02_Day_Variables_builtin_functions/day02.py
@@ -0,0 +1,66 @@
+#Day2
+
+first_name = 'Alphonso'
+last_name = 'Williams'
+full_name = first_name + ' ' + last_name
+country = 'Japan'
+city = 'Nago'
+age = 25
+year = 2026
+is_married = False
+is_true = True 
+is_light_on = True
+first_name, last_name, age, year = 'Alphonso', 'Williams', 25, 2026
+
+print('first_name:', first_name)
+print('last_name:', last_name)
+print('full_name:', full_name)
+print('country:', country)
+print('city:', city)
+print('age:', age)
+print('year:', year)
+print('Is married?', is_married)
+print('Is true?', is_true)
+print('Is light on?', is_light_on)
+print(first_name, last_name, age, year)
+
+#Data Types 
+print('first_name:', len(first_name))
+print('last_name:', len(last_name))
+print(len(first_name) == len(last_name))
+num_one = 5 
+num_two = 4
+
+total = num_one + num_two
+print('Total:', total)
+
+diff = num_two - num_one
+print('Difference:', diff)
+
+product = num_two * num_one
+print('Product:', product)
+
+division = num_one / num_two
+print('Division:', division)
+
+variable_remainder = num_two % num_one
+print('Remainder:', variable_remainder)
+
+variable_exp = num_one ** num_two
+print('Exponent:', variable_exp)
+
+floor_division = num_one // num_two
+print('Floor Division:', floor_division)
+
+radius = 30
+area_of_circle = 3.14 * radius ** 2
+print('Area of Circle:', area_of_circle)
+
+circum_of_circle = 2 * 3.14 * radius 
+print('Circumference of Circle:', circum_of_circle)
+
+user_radius = int(input('Enter radius: '))
+user_area = 3.14 * user_radius ** 2
+print('Area of Circle:', user_area)
+
+
